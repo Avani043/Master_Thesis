92f6da79fe221628a881d8fa7051ee33d3b98492
Avani043 <avanigandhi321@gmail.com>
2025-08-12 20:50:04 +0200

    run webmall study with 4.1 mini - flags_AX_M

--- WebMall/run_webmall_study.py
+++ WebMall/run_webmall_study.py
@@ -112,11 +112,16 @@ AGENT_41m_V = GenericAgentArgs(
     flags=FLAGS_V,
 )
 
-
 AGENT_41m_AX_V = GenericAgentArgs(
     chat_model_args=CHAT_MODEL_ARGS_DICT["openai/gpt-4.1-mini-2025-04-14"],
     flags=FLAGS_AX_V,
 )
+AGENT_41m_AX_M = GenericAgentArgs(
+    chat_model_args=CHAT_MODEL_ARGS_DICT["openai/gpt-4.1-mini-2025-04-14"],
+    flags=FLAGS_AX_M,
+)
+
+
 
 AGENT_CLAUDE_AX = GenericAgentArgs(
     chat_model_args=CHAT_MODEL_ARGS_DICT["anthropic/claude-sonnet-4-20250514"],
@@ -160,7 +165,7 @@ load_dotenv(PATH_TO_DOT_ENV_FILE)
 
 # choose your agent or provide a new agent
 #agent_args = [AGENT_41_AX]
-agent_args = [AGENT_4om_AX_V]
+agent_args = [AGENT_41m_AX_M]
 
 # ## select the benchmark to run on
 
